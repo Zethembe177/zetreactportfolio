c044c259e76e92d40438a1ff65643b86c95036d5
unknown <nxumalozethembe@gmail.com>
2025-07-25 11:23:19 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Skills from './components/Skills';
+import Home from './components/Home';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home/>
+      <About />
+      <Skills />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
